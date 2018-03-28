96c1acedb805a4a7d0b5260aeb49a37accdecf85
clayding <fancyou@yeah.net>
2018-03-28 17:38:12 +0800

    添加CSIG的文件，并在.h中添加一些宏定义

---
+++ Driver/Inc/rh850f1l_csi.h
@@ -0,0 +1,102 @@
+/**
+  ******************************************************************************
+  * @file    rh850f1l_csi.h
+  * @author  Automotive Business Division
+  * @version V1.0.0
+  * @date    28-March-2018
+  * @brief   Header file of CSIG module.
+  ******************************************************************************
+  */
+#ifndef RH850F1L_RLIN_H
+#define RH850F1L_RLIN_H
+#include "rh850f1l.h"
+
+
+/*CSIGnCTL0 — CSIGn Control Register 0*/
+#define CSI_CSIGnPWR_OFFSET         7 //Controls operation clock.
+#define CSI_CSIGnTXE_OFFSET         6 //Enables/disables transmission.
+#define CSI_CSIGnRXE_OFFSET         5 //Enables/disables reception
+
+#define CSI_CSIGnPWR_MASK           ((uint8_t)(0x01 << CSIG_CSIGnPWR_OFFSET))
+#define CSI_CSIGnTXE_MASK           ((uint8_t)(0x01 << CSIG_CSIGnTXE_OFFSET))
+#define CSI_CSIGnRXE_MASK           ((uint8_t)(0x01 << CSIG_CSIGnRXE_OFFSET))
+#define CSI_CSIGnMBS_MASK           ((uint8_t)0x01)//This bit must be set to 1 (the value after reset is 0)
+
+/*CSIGnCTL1 — CSIGn Control Register 1*/
+#define CSI_CSIGnCKR_OFFSET         17 //CSIGTSCK clock inversion function
+#define CSI_CSIGnSLIT_OFFSET        16 //Selects the timing of interrupt INTCSIGTIC
+#define CSI_CSIGnEDLE_OFFSET        7  //Enables/disables extended data length (EDL) mode
+#define CSI_CSIGnDCS_OFFSET         5  //Enables/disables data consistency check
+#define CSI_CSIGnLBM_OFFSET         3  //Controls loop-back mode (LBM)
+#define CSI_CSIGnSIT_OFFSET         2  //Selects interrupt delay mode
+#define CSI_CSIGnHSE_OFFSET         1 //Enables/disables handshake function
+
+#define CSI_CSIGnCKR_MASK           ((uint32_t)(0x01 << CSIG_CSIGnCKR_OFFSET))
+#define CSI_CSIGnSLIT_MASK          ((uint32_t)(0x01 << CSIG_CSIGnSLIT_OFFSET))
+#define CSI_CSIGnEDLE_MASK          ((uint32_t)(0x01 << CSIG_CSIGnEDLE_OFFSET))
+#define CSI_CSIGnDCS_MASK           ((uint32_t)(0x01 << CSIG_CSIGnDCS_OFFSET))
+#define CSI_CSIGnLBM_MASK           ((uint32_t)(0x01 << CSIG_CSIGnLBM_OFFSET))
+#define CSI_CSIGnSIT_MASK           ((uint32_t)(0x01 << CSIG_CSIGnLBM_OFFSET))
+#define CSI_CSIGnHSE_MASK           ((uint32_t)(0x01 << CSIG_CSIGnSIT_OFFSET))
+#define CSI_CSIGnSSE_MASK           ((uint32_t)0x01) //Enables/disables slave select function
+
+/*CSIGnCTL2 — CSIGn Control Register 2*/
+#define CSI_CSIGnPRS_OFFSET         13  //
+
+#define CSI_CSIGnPRS_MASK           ((uint16_t)(0x07 << CSIG_CSIGnPRS_OFFSET))
+#define CSI_CSIGnBRS_MASK           ((uint16_t)0xFFF) //Selects the transfer clock frequency
+
+/*CSIGnSTR0 — CSIGn Status Register 0  read-only*/
+#define CSI_CSIGnTSF_OFFSET         7 //Transfer Status Flag
+#define CSI_CSIGnDCE_OFFSET         3 //Data Consistency Check Error Flag
+#define CSI_CSIGnPE_OFFSET          1 //Parity Error Flag
+
+#define CSI_CSIGnTSF_MASK           ((uint32_t)(0x01 << CSIG_CSIGnTSF_OFFSET))
+#define CSI_CSIGnDCE_MASK           ((uint32_t)(0x01 << CSIG_CSIGnDCE_OFFSET))
+#define CSI_CSIGnPE_MASK            ((uint32_t)(0x01 << CSIG_CSIGnPE_OFFSET))
+#define CSI_CSIGnOVE_MASK           ((uint32_t)0x01)//Overrun Error Flag
+
+/*CSIGnSTCR0 — CSIGn Status Clear Register 0*/
+#define CSI_CSIGnDCEC_OFFSET        3 //Controls the data consistency error flag clear command
+#define CSI_CSIGnPEC_OFFSET         1 //Controls the parity error flag clear command
+
+#define CSI_CSIGnDCEC_MASK          ((uint16_t)(0x01 << CSI_CSIGnDCEC_OFFSET))
+#define CSI_CSIGnPEC_MASK           ((uint16_t)(0x01 << CSI_CSIGnPEC_OFFSET))
+#define CSI_CSIGnOVEC_MASK          ((uint16_t)0x01) //Controls the overrun error flag clear command
+
+/*CSIGnBCTL0 — CSIGn Rx-Only Mode Control Register 0*/
+#define CSI_CSIGnSCE_MASK           ((uint18_t)0x01) //Disables/enables the start of the next data reception by reading CSIGnRX0
+
+/*CSIGnCFG0 — CSIGn Configuration Register 0*/
+#define CSI_CSIGnPS_OFFSET          28 //Specifies parity
+#define CSI_CSIGnDLS_OFFSET         24 //Specifies data length
+#define CSI_CSIGnDIR_OFFSET         18 //Selects the serial data direction.
+#define CSI_CSIGnDAP_OFFSET         16 //Data Phase Selection
+
+#define CSI_CSIGnPS_MASK            ((uint32_t)(0x03 << CSI_CSIGnPS_OFFSET))
+#define CSI_CSIGnDLS_MASK           ((uint32_t)(0x0F << CSI_CSIGnDLS_OFFSET))
+#define CSI_CSIGnDIR_MASK           ((uint32_t)(0x01 << CSI_CSIGnDIR_OFFSET))
+#define CSI_CSIGnDAP_MASK           ((uint32_t)(0x01 << CSI_CSIGnDAP_OFFSET))
+
+/*CSIGnTX0W — CSIGn Transmission Register 0 for Word Access*/
+#define CSI_CSIGnEDL_OFFSET         29 //Specifies the extended data length
+
+#define CSI_CSIGnEDL_MASK           ((uint32_t)(0x03 << CSI_CSIGnEDL_OFFSET))
+#define CSI_CSIGnTX_MASK            ((uint32_t)0xFF)//Data to be transmitted
+
+/*CSIGnTX0H — CSIGn Transmission Register 0 for Half Word Access*/
+//#define CSI_CSIGnTX_MASK            ((uint32_t)0xFF) //duplicated with the CSIGnTX0W
+
+/*CSIGnRX0 — CSIGn Reception Register 0  read-only*/
+#define CSI_CSIGnRX_MASK            ((uint32_t)0xFF) //Received Data
+
+/*CSIGnEMU — CSIGn Emulation Register*/
+#define CSI_CSIGnSVSDIS_OFFSET      7
+#define CSI_CSIGnSVSDIS_MASK        ((uint8_t)0x01 << CSI_CSIGnSVSDIS_OFFSET)
+
+#define CSIGN_ADDR(_UNIT_)          ((volatile struct __tag36 *)(((uint8_t*)(&_CSIG0))+  0x2000*(_UNIT_)))
+#define CSIGN_VAL(_UNIT_)           (*((volatile struct __tag36 *)(((uint8_t*)(&_CSIG0))+  0x2000*(_UNIT_))))
+
+/*CSIGnCTL0 — CSIGn Control Register 0*/
+#define __CSIG_SET_CTL0(_N_,_MASK_VAULE_)       MODIFY_REG(CSIGN_ADDR(_N_),_MASK_,_VALUE_)
+#define 
